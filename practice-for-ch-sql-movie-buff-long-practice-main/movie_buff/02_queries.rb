e5de582cfccc803edd0c28696e048978d1dd97a4
Sean <pca.sean.jeun@gmail.com>
2023-07-28 15:53:03 -0400

    vanity project built

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -1,18 +1,27 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
-  
+  Movie
+    .where(yr: 1980..1989, score: 3..5)
+    .select(:id, :title, :yr, :score)
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
-  
+  Movie
+    .group(:yr)
+    .having('MAX(score) < 8')
+    .pluck(:yr)
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+  Actor
+    .joins(:movies)
+    .where(movies: {title: title})
+    .order(ord: :asc)
+    .select(:id, :name)
 end
 
 def vanity_projects
@@ -20,7 +29,10 @@ def vanity_projects
   # starring actor. Show the movie id, title, and director's name.
 
   # Note: Directors appear in the 'actors' table.
-  
+  Movie
+    .joins(:actors)
+    .where('director_id = actor_id AND ord = 1')
+    .select(:id, :title, :name)
 end
 
 def most_supportive
